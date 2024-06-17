e89e75ea107f55ec78172fd3ba6fdb45210190d0
Musul Man <musulmanco@gmail.com>
2024-06-17 12:48:46 -0300

    actualizacion de registro

---
+++ assets/javascript/registro.js
@@ -0,0 +1,5 @@
+const ingreseUsuario = document.getElementById("idIngreseUsuario");
+const ingreseContrasenia = document.getElementById("idIngreseContrasenia");
+const ingreseRepetirContrasenia = document.getElementById(
+  "idIngreseRepetirContrasenia"
+);
